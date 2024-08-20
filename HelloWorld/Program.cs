50c9d26ab4285a3c1d4ebba9699cb55d1f55e97e
tdonnelly1102 <58100933+tdonnelly1102@users.noreply.github.com>
2024-08-20 22:22:46 +0000

    Created a Hello World program in code spaces

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Toast is good");
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("i = " + i);
+        }
+    }
+}
